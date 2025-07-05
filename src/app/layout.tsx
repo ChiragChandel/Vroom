48627fd2acbbc114a330f7f90be47bd6deae4863
ChiragChandel <chiragchandel10@gmail.com>
2025-07-06 00:04:29 +0530

    fix: light/dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import '../styles/tailwind.css';
 import Layout from '@/components/Layout';
-
+import './globals.css'
 export const metadata = {
   title: 'Vroom - Car Blog',
   description: 'Explore car blogs, reviews, and specs.',
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body >
         <Layout>{children}</Layout>
       </body>
     </html>
